28ff17b9936ecf69d790d8c45b0b3119303104cc
Shane Swiderek <s.swiderek@gmail.com>
2025-02-21 03:44:37 +0000

    Restored to '96baa4c189ad125d619604b56c980ff0e2c1461e'
    Replit-Restored-To:96baa4c189ad125d619604b56c980ff0e2c1461e

--- client/src/components/calculator-form.tsx
+++ client/src/components/calculator-form.tsx
@@ -452,8 +452,8 @@ export default function CalculatorForm() {
             <div className="h-full flex flex-col items-center justify-center gap-2 text-muted-foreground -mt-8">
               <div className="ship-container relative">
                 <img
-                  src="/images/bulk_carrier.png"
-                  alt="Bulk Carrier"
+                  src="/images/cargo-ship.png"
+                  alt="Cargo Ship"
                   className="w-48 h-48 object-contain mb-3"
                 />
                 <div className="waves-small"></div>
